2c47d00f6312d9aed3980fea693e4c45c58e9bf2
Liza <elizavetanakasone@gmail.com>
2023-06-08 19:07:57 +0900

    Решение задачи

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arrA = new string[4] {"Hello", "2", "world", ":-)"};
+string[] arrB = new string[arrA.Length];
+
+void ArrayAfterSelection(string[] arrA, string[] arrB)
+{
+    int count = 0;
+    for (int i = 0; i < arrA.Length; i++)
+    {
+    if(arrA[i].Length <= 3)
+        {
+        arrB[count] = arrA[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayAfterSelection(arrA, arrB);
+PrintArray(arrB);
